854ed0bebf3cf18fa4d91e239878b9dfa6e718c7
vilal <vilal@DESKTOP-USVAAMB>
2020-07-22 18:08:12 -0400

    meu primeiro commit dentro do netbenas

--- src/TesteSistemaLojas.java
+++ src/TesteSistemaLojas.java
@@ -9,7 +9,10 @@ public class TesteSistemaLojas
     PreparedStatement pst = null;
     ResultSet rs = null;
     
-  public static void main(String[] args)
+    /**
+     *
+     */
+    public TesteSistemaLojas()
   {
    Janela janela = new Janela("Sistema de Lojas");
    conexao = ModuloConexao.conector();
