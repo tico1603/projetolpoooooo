63fe19ac229611071d921cf18526d27b0c1b0d16
vilal <vilal@DESKTOP-USVAAMB>
2020-07-22 17:58:59 -0400

    meu primeiro commit dentro do netbenas

--- src/projetolpoooooo/TesteSistemaLojas.java
+++ src/projetolpoooooo/TesteSistemaLojas.java
@@ -1,7 +1,18 @@
+import java.sql.*;
+import projetolpoooooo.ModuloConexao;
+
+
 public class TesteSistemaLojas
 {
-  public static void main(String[] Args)
+    
+    Connection conexao = null;
+    PreparedStatement pst = null;
+    ResultSet rs = null;
+    
+  public void main(String[] args)
   {
    Janela janela = new Janela("Sistema de Lojas");
+   conexao = ModuloConexao.conector();
+      System.out.println("conexao");
   }
 }
