c8b6109f7bf4c6f6cb14e8e97b38d61a4dd685cf
vilal <vilal@DESKTOP-USVAAMB>
2020-07-22 23:13:46 -0400

    meu primeiro commit dentro do netbenas

--- src/projetolpoooooo/ModuloConexao.java
+++ src/projetolpoooooo/ModuloConexao.java
@@ -5,24 +5,35 @@
  */
 package projetolpoooooo;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author vilal
  */
 public class ModuloConexao {
-    public static Connection conector(){
-        java.sql.Connection conexao = null;
-        String driver = "com.mysql.jdbc.Driver";
-        String url = "jdbc:mysql://localhost:3306/dbProjetoLpoo";
-        String user ="root";
-        String password = "";
-        try{
-            Class.forName(driver);
-            conexao = DriverManager.getConnection(url, user, password);
-            return conexao;
-        } catch (Exception e){
-            return null;
+
+    private static Connection conexao;
+
+    public static Connection getConexao() {
+        if (conexao == null){
+            try {
+                conexao = DriverManager.getConnection("jdbc:mysql://192.168.2.3:3308/compraonline" + "?serverTimezone=UTC&autoReconnect=true&useSLL=false", "root", "root");
+            } catch (SQLException ex) {
+                System.out.println("Erro ao conectar no BD.");
+                Logger.getLogger(ModuloConexao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+            
+        return conexao;
     }
+    
+    
+    
+       
+    
 }
--- src/projetolpoooooo/TesteSistemaLojas.java
+++
@@ -1,7 +0,0 @@
-public class TesteSistemaLojas
-{
-  public static void main(String[] Args)
-  {
-   Janela janela = new Janela("Sistema de Lojas");
-  }
-}
